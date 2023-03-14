c60c9c7b00a8379711d3d230fde906a6d5e6fc51
juniffiro <juniffirowork@gmail.com>
2023-03-14 12:51:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DLL-Entrypoint
+Example
---
+++ src/dllmain.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+
+DWORD WINAPI TestThread() {
+    MessageBoxA(NULL, "Hello world!", "Test window", MB_OK);
+    return TRUE;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)TestThread, NULL, NULL, NULL);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
